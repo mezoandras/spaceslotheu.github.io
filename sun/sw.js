593ecfe49101d2170520f7296432232fbb50b843
Mezo Andras <47240376+mezoandras@users.noreply.github.com>
2026-08-21 23:20:47 +0200

    Add files via upload

--- sun/sw.js
+++ sun/sw.js
@@ -3,7 +3,7 @@
    Minden hivatkozás relatív, így a mappa bárhova másolható.
    Frissítéskor emeld a VERSION számot, különben a telefon a régit tartja meg. */
 
-const VERSION = "felirat-v3";
+const VERSION = "felirat-v4";
 const SHELL = [
   "./",
   "./index.html",
